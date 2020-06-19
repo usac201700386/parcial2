bcce5187e9c880dbd5c7235ba8fe351cc7d37a32
usac201700804 <2872817520401@ingenieria.usac.edu.gt>
2020-06-19 17:56:20 -0600

    prueba3 MQTT

--- servidor.py
+++ servidor.py
@@ -1,4 +1,5 @@
 import logging
+import os
 import paho.mqtt.client as mqtt
 from credenciales import *
 
@@ -18,16 +19,41 @@ def on_publish(client, userdata, mid):
     publishText = "Publicacion satisfactoria"
     logging.debug(publishText)
 
+#Callback que se ejecuta cuando llega un mensaje al topic suscrito
+def on_message(client, userdata, msg):
+    #Se muestra en pantalla informacion que ha llegado
+    logging.info('mensaje recibido: ' + str(msg.payload))
+
 '''
 Config. inicial del cliente MQTT
 '''
 server_MQTT = mqtt.Client(clean_session=True) #Nueva instancia de cliente
 server_MQTT.on_connect = on_connect #Se configura la funcion "Handler" cuando suceda la conexion
 server_MQTT.on_publish = on_publish #Se configura la funcion "Handler" que se activa al publicar algo
+server_MQTT.on_message = on_message #Se configura la funcion "Handler" que se activa al llegar un mensaje a un topic subscrito
 server_MQTT.username_pw_set(MQTT_USUARIO, MQTT_KEY) #Credenciales requeridas por el broker
 server_MQTT.connect(host = HOST, port = MQTT_PORT) #Conectar al servidor remoto
 
-server_MQTT.publish('comandos/04', "/x01", qos = 0, retain = False)
+topico = 'comandos/04'
+mensaje = b'/x01'
 
-server_MQTT.disconnect()
-logging.info("Se ha desconectado del broker. Saliendo...")
+#Publicacion simple
+server_MQTT.publish(topico, mensaje, qos = 0, retain = False)
+
+#Subscripcion simple con tupla (topic,qos)
+server_MQTT.subscribe((topico, 0))
+
+#Iniciamos el thread (implementado en paho-mqtt) para estar atentos a mensajes en los topics subscritos
+server_MQTT.loop_start()
+
+
+try:
+    while True:
+        pass
+
+except KeyboardInterrupt:
+    logging.info('Desconectando...')
+
+finally:
+    server_MQTT.disconnect()
+    logging.info('Se ha desconectado!')
