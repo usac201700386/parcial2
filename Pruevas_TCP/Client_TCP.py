79d3f51fb75489154716a39744a7e3fb60c595fe
usac201700386 <3006775370101@ingenieria.usac.edu.gt>
2020-06-20 16:44:08 -0600

    actualizacion comunicacion en ambos sentidos

--- Pruevas_TCP/Client_TCP.py
+++ Pruevas_TCP/Client_TCP.py
@@ -15,7 +15,7 @@ logging.basicConfig(
     format = '[%(levelname)s] (%(threadName)-10s) %(message)s'
     )
 
-def est_connect(hots='167.71.243.238',port=9804):
+def est_connect(host='167.71.243.238',port=9804):
     #JDCP se crea el socket a utilizar
     sock = socket.socket()
     #JDCP se indica se que esta intentando conectar al servidor
@@ -33,7 +33,7 @@ def Audio_create(filename='audio.wav',duracion=3):
     filesize = os.path.getsize(filename)
     return filename , filesize
     
-def Envio_TCP (filename='audio.wav',filesize=1024,host='167.71.243.238',port=9804):
+def Envio_TCP_Client (filename='audio.wav',filesize=1024,host='167.71.243.238',port=9804):
  
     #se manda la informacion del comando FTR , destino y peso del archivo
     SEPARATOR = "<SEPARATOR>" # solamente servira para dierencia la informacion comandos 
@@ -46,7 +46,6 @@ def Envio_TCP (filename='audio.wav',filesize=1024,host='167.71.243.238',port=980
         for i in progress:
             # read the bytes from the file
             bytes_read = f.read(filesize)
-            
             if not bytes_read:
                 # file transmitting is done
             
@@ -60,10 +59,12 @@ def Envio_TCP (filename='audio.wav',filesize=1024,host='167.71.243.238',port=980
     # close the socket
     sock.close()
 
-def Recp_TCP (host='167.71.243.238',port=9804):
+def Recp_TCP_Client (host='167.71.243.238',port=9804):
     #JDCP ESTABLECIENDO COMUNICAICOND CON EL SERVIDOR PARA RECIBIR ARCHIVOS
+    time.sleep(0.05)
     sock= est_connect(host,port)
     received = sock.recv(BUFFER_SIZE).decode()
+    SEPARATOR = "<SEPARATOR>" 
     filename, filesize = received.split(SEPARATOR)
     # remove absolute path if there is
     filename = 'REB_CLIENTE.wav'#os.path.basename(filename)
@@ -88,6 +89,17 @@ def Recp_TCP (host='167.71.243.238',port=9804):
     #JDCP se desconecta de cliente TCP al recibir el archivo
     sock.close()
 
-nombre,tamano=Audio_create('audio.wav',5)
+#nombre,tamano=Audio_create('audio.wav',5)
 
-Envio_TCP(nombre,tamano,host,port)
+#Envio_TCP_Client(nombre,tamano,host,port)
+
+while True:
+    logging.debug('ingrese 1 para envio, 2 para recepcion')
+    entrada = int(input('ingrese su seleccion: '))
+    if (entrada ==1):
+        nombre,tamano=Audio_create('audio.wav',5)
+        Envio_TCP_Client(nombre,tamano,host,port)
+    elif (entrada == 2):
+        Recp_TCP_Client(host,port)   
+    else:
+        break
--- Pruevas_TCP/Server_TCP.py
+++ Pruevas_TCP/Server_TCP.py
@@ -2,54 +2,99 @@ import socket
 import tqdm
 import os
 import time
-#delay=0.0025
-# device's IP address
-SERVER_HOST = 'localhost'#"167.71.243.238"
-SERVER_PORT = 9800#9804#5001
-# receive 4096 bytes each time
+import logging
+#JDCP  estos datos se tienen que importar desde un archivo de texto plano
+SERVER_HOST = 'localhost'#"167.71.243.238" #host a conectar
+SERVER_PORT = 9800#9804# numero de puerto a utlizar
 BUFFER_SIZE = 64 *1024
+#este carcter es propio del prama para obtener informacion
 
-SEPARATOR = "<SEPARATOR>"
+#Configuracion inicial para logging. logging.DEBUG muestra todo.
+logging.basicConfig(
+    level = logging.DEBUG, 
+    format = '[%(levelname)s] (%(threadName)-10s) %(message)s'
+    )
+#JDCP creando el TCP socket
+def est_servidor(host='167.71.243.238',port=9804):
+    #JDCP SE CREAR EL OBJETO TIPO SOCKET
+    servidor = socket.socket()
+    #JDCP SE LEVANTA EL SERVIDOR TCP LISTO PARA ESCUCHA
+    servidor.bind((host,port))
 
-# create the server socket
-# TCP socket
-s = socket.socket()
+    servidor.listen(5)
+    logging.debug(f"[*] Esperando conexion en {SERVER_HOST}:{SERVER_PORT}")
 
-s.bind((SERVER_HOST, SERVER_PORT))
+    client_socket, address = servidor.accept() # datos el 
+    # if below code is executed, that means the sender is connected
+    logging.debug(f"[+] {address} estado conectado")
+    return servidor,client_socket,address
 
-s.listen(5)
-print(f"[*] Listening as {SERVER_HOST}:{SERVER_PORT}")
+def Recp_TCP_Server(host='167.71.243.238',port=9804):
 
-client_socket, address = s.accept() 
-# if below code is executed, that means the sender is connected
-print(f"[+] {address} is connected.")
+    server,cliente,direccion = est_servidor(host,port)
 
-received = client_socket.recv(BUFFER_SIZE).decode()
-filename, filesize = received.split(SEPARATOR)
-# remove absolute path if there is
-filename = 'REB_server.wav'#os.path.basename(filename)
-# convert to integer
-filesize = int(filesize)
+    received = cliente.recv(BUFFER_SIZE).decode()
+    SEPARATOR = "<SEPARATOR>"
 
-progress = tqdm.tqdm(range(filesize), f"Receiving {filename}", total=1,unit="B", unit_scale=True, unit_divisor=0.1)
-with open(filename, "wb") as f:
-    for i in progress:
-        # read 1024 bytes from the socket (receive)
-        bytes_read = client_socket.recv(filesize)
-        
-        if not bytes_read:    
-            # nothing is received
-            # file transmitting is done
+    filename, filesize = received.split(SEPARATOR)
+    # remove absolute path if there is
+    filename = 'Recp_server.wav'#os.path.basename(filename)
+    # convert to integer
+    filesize = int(filesize)
+
+    progress = tqdm.tqdm(range(filesize), f"Receiving {filename}", total=1,unit="B", unit_scale=True, unit_divisor=0.1)
+    with open(filename, "wb") as f:
+        for i in progress:
+            # read 1024 bytes from the socket (receive)
+            bytes_read = cliente.recv(filesize)
             
-            break
-        # write to the file the bytes we just received
-        f.write(bytes_read)
-        #time.sleep(delay)
-        # update the progress bar
-    progress.update(i)
-        
+            if not bytes_read:    
+                # nothing is received
+                # file transmitting is done
+                
+                break
+            # write to the file the bytes we just received
+            f.write(bytes_read)
+            #time.sleep(delay)
+            # update the progress bar
+        progress.update(i)
+            
 
-# close the client socket
-client_socket.close()
-# close the server socket
-s.close()
+    # close the client socket
+    cliente.close()
+    # close the server socket
+    server.close()
+def Envio_TCP_Server(host='167.71.243.238',port=9804):
+    servidor,cliente,direccion= est_servidor(host,port)
+    filename= 'Recp_server.wav'
+    SEPARATOR = "<SEPARATOR>"
+    filesize = os.path.getsize(filename)
+    cliente.send(f"{filename}{SEPARATOR}{filesize}".encode())
+    # if comando ok 
+    progress = tqdm.tqdm(range(filesize),f"Sending {filename}",total=1,miniters=0.01,mininterval=0.001 ,unit="B", unit_scale=False, unit_divisor=1024)
+    with open(filename, "rb") as f:
+        for i in progress:
+            # read the bytes from the file
+            bytes_read = f.read(filesize)
+            if not bytes_read:
+                # file transmitting is done
+            
+                break
+            # we use sendall to assure transimission in 
+            # busy networks
+            cliente.sendall(bytes_read)
+            #time.sleep(delay)
+            # update the progress bar
+            progress.update(len(bytes_read))         
+    # close the socket
+    cliente.close()
+    servidor.close()
+while True:
+    logging.debug('ingrese 1 para recibir, 2 para enviar')
+    entrada = int(input('ingrese su seleccion: '))
+    if (entrada ==1):
+        Recp_TCP_Server(SERVER_HOST,SERVER_PORT)
+    elif (entrada == 2):
+        Envio_TCP_Server(SERVER_HOST,SERVER_PORT)
+    else:
+        break
