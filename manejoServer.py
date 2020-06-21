3024827788d71bdacd5cca4488496ec3d0a2d4c2
usac201700804 <2872817520401@ingenieria.usac.edu.gt>
2020-06-20 21:55:46 -0600

    manejoServer 1.0

--- manejoServer.py
+++ manejoServer.py
@@ -1,8 +1,12 @@
 import paho.mqtt.client as mqtt
+import socket
+import tqdm
 import logging
+import os
 from globals import *
 
 class Servidor():
+    #----------------------------------------------   DAHM MQTT   -------------------------------------------------
     #DAHM Metodo constructor del objeto de la clase servidor, este metodo toma las constantes globales necesarias para:
     #DAHM crear una instancia del tipo mqtt.Client, una de tipo socket y los archivos de texto donde se va a llevar el
     #DAHM control de usuarios y salas.
@@ -25,6 +29,7 @@ class Servidor():
         self.salas = salas
         #DAHM Datos del grupo
         self.grupo = grupo
+        
 
     #DAHM Configuracion inicial para que el servidor se vuelva subscriptor y publicador en el broker
     def configMQTT(self):
@@ -62,7 +67,14 @@ class Servidor():
     #DAHM metodos del servidor
     def subscripcion(self):
         client = self.x
-        topicos = []
+        topicos_l = self.topicos()
+        #DAHM Ya que se tiene una lista de tuplas con todos los elementos de los archivos de texto se subscribe el servidor
+        client.subscribe(topicos_l[:])
+        #DAHM Se comienza el thread demonio que ve si llega algun mensaje a alguno de estos topicos
+        client.loop_start()
+
+    def topicos(self):
+        topicos_lista = []
         #DAHM Ahora que ya hay certeza de que existen los archivos se ponen en modo lectura.
         archivo_usuarios = open(self.usuarios, 'r')
         archivo_salas = open(self.salas, 'r')
@@ -70,20 +82,19 @@ class Servidor():
         for usuario in archivo_usuarios:
             t = usuario.split(',')
             topico = ('usuarios' + '/' + t[0], self.qos)
-            topicos.append(topico)
+            topicos_lista.append(topico)
         archivo_usuarios.close()
         #DAHM Este otro bucle toma cada linea del archivo salas y guarda cada topico en el formato indicado
         for sala in archivo_salas:
             sala = sala.split('\n')
             topico = ('salas' + '/' + self.grupo + '/' + sala[0][2:], self.qos)
-            topicos.append(topico)
+            topicos_lista.append(topico)
         archivo_salas.close()
         #DAHM Se agrega comandos a la lista de tuplas
-        topicos.append(('comandos/14/#', self.qos))
-        #DAHM Ya que se tiene una lista de tuplas con todos los elementos de los archivos de texto se subscribe el servidor
-        client.subscribe(topicos[:])
-        #DAHM Se comienza el thread demonio que ve si llega algun mensaje a alguno de estos topicos
-        client.loop_start()
+        topicos_lista.append(('comandos/' + self.grupo + '/#', self.qos))
+
+        return topicos_lista
+
 
     #DAHM Esta funcion es la analogia de publish de paho
     def publicar(self, topico, mensaje):
@@ -99,6 +110,81 @@ class Servidor():
     def desconectar(self):
         client = self.x
         client.disconnect()
+
+    #-----------------------------------------------------------------------------------
+    #------------JDCP TCP----------------
+    #JDCP creando el TCP socket
+    def est_servidor(self):
+        #JDCP SE CREAR EL OBJETO TIPO SOCKET
+        servidor = socket.socket()
+        #JDCP SE LEVANTA EL SERVIDOR TCP LISTO PARA ESCUCHA
+        servidor.bind((self.ip,self.portTCP))
+
+        servidor.listen(5)
+        logging.debug(f"[*] Esperando conexion en {self.ip}:{self.portTCP}")
+        #JDCP se obtine la informacion del cliente que se conecto al servicio
+        client_socket, address = servidor.accept() # datos el 
+        #JDCP SE AVISA POR MEDIO DE LOGGING QUE SE HA CONECTADO AL SERVIDOR
+        logging.debug(f"[+] {address} estado conectado")
+        return servidor,client_socket,address
+
+    def Recp_TCP_Server(self):
+        #JDCP se obtiene todos los parametros y objetos de la funcion que levanta el servidor TCP
+        server,cliente,_ = self.est_servidor()
+        #SE ESTA ATENTO A LA INFORMACION QUE SE RECIBE DEL TCP SOBRE EL TAMANO DEL ARCHIVO
+        received = cliente.recv(64*1024).decode()
+        SEPARATOR = "<SEPARATOR>"
+        #SE ESTABLECE EL NOMBRE DEL ARCHIVO QUE SE ALMACENA EN EL servido
+        filename, filesize = received.split(SEPARATOR)
+    
+        filename = 'Recp_server.wav'#os.path.basename(filename)
+        #JDCP CONVERITR EL VALOR DEL TAMANO DE STRING A ENTERO
+        filesize = int(filesize)
+
+        progress = tqdm.tqdm(range(filesize), f"Receiving {filename}", total=1,unit="B", unit_scale=True, unit_divisor=0.1)
+        with open(filename, "wb") as f:
+            for i in progress:
+                #JDCP LEE LA CANTIDA DE BYTES DEL TAMANO DEL ARCHIVO
+                bytes_read = cliente.recv(filesize)
+                
+                if not bytes_read:    
+                    # nothing is received
+                    # file transmitting is done
+                    break
+                # JDCP SE ESCRIBE EL EL ARCHIVO QUE SE ESTA RECIBIENDO
+                f.write(bytes_read)
+            progress.update(i)
+        #JDCP SE CIERRA LA COMUNICACION CON EL CLIENTE
+        cliente.close()
+        #JDCP SE CIERRA EL SERVIDOR
+        server.close()
+        
+    def Envio_TCP_Server(self):
+
+        servidor,cliente,_= self.est_servidor()
+        filename= 'Recp_server.wav'
+        SEPARATOR = "<SEPARATOR>"
+        filesize = os.path.getsize(filename)
+        cliente.send(f"{filename}{SEPARATOR}{filesize}".encode())
+        
+        progress = tqdm.tqdm(range(filesize),f"Sending {filename}",total=1,miniters=0.01,mininterval=0.001 ,unit="B", unit_scale=False, unit_divisor=1024)
+        with open(filename, "rb") as f:
+            for _ in progress:
+                #JDCP SE LEE LOS BYTES DEL ARCHIVO DE AUDIO
+                bytes_read = f.read(filesize)
+                if not bytes_read:
+                    #JDCP ESTO SUCEDE CUANDO FINALIZA EL ENVIO 
+                
+                    break
+                #JDCP ESTO SUCEDE MINETRA SE ENVIA CADA BYTE DE AUDIO
+                cliente.sendall(bytes_read)
+            # JDCP SE ACTULIZA EL VALOR DE LA BARRA QUE SE ESTA MOSTRANDO
+                progress.update(len(bytes_read))         
+        # JDCP SE CORTA COMUNICACION CON EL CLIENTE Y SE CIERRA EL SERVIDOR TCP
+        cliente.close()
+        servidor.close()
+    
+    #-----------------------------------------------------------------------------------
 
     
 
--- servidor.py
+++ servidor.py
@@ -7,9 +7,16 @@ server.configMQTT()
 server.conectar()
 server.subscripcion()
 
+topics = server.topicos()
+for topic in topics:
+    print(topic[0])
+
 try:
     while True:
-        pass
+        destino = input('Ingrese destinatario: ')
+        mensaje = input('Ingrese mensaje: ')
+        server.publicar(destino, mensaje)
+
 
 except KeyboardInterrupt:
     logging.info('desconectado del broker!')
