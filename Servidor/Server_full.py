828a1aefd9aecb4adb1714fe63f9033a7b56e8a9
usac201700386 <3006775370101@ingenieria.usac.edu.gt>
2020-06-22 09:43:01 -0600

    fix errors

--- Servidor/Server_full.py
+++ Servidor/Server_full.py
@@ -100,7 +100,7 @@ class Servidor():
                 if (ID not in self.lista_actual):
                     self.lista_actual.append(ID)
                 else: 
-                    pass
+                    self.lista_actual=[]
                 
                     
 
