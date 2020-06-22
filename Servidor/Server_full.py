0c0359e730bdc8385ff50cbc864ed35d45529343
usac201700386 <3006775370101@ingenieria.usac.edu.gt>
2020-06-22 10:05:37 -0600

    bug

--- Servidor/Server_full.py
+++ Servidor/Server_full.py
@@ -320,7 +320,8 @@ def control_alive(lista_conectados=[],lista_actual=[]):
     if(len(lista_conectados)==3):
         lista_conectados.remove(lista_conectados[0])
     lista_conectados.append(lista_actual)
-    lista_actual[:]=[]
+    if (len(lista_actual)>0):
+        lista_actual[:]=[]
     logging.debug('lista de conectados -> '+str(lista_conectados))
     
     
